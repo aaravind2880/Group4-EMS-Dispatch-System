612a85198336704d1c6939e51d0593680b79bb66
AaKhemani <aarav.khemani@student.uts.edu.au>
2026-04-25 13:01:12 +1000

    Add files via upload

---
+++ Prototype/src/main.tsx
@@ -0,0 +1,7 @@
+
+  import { createRoot } from "react-dom/client";
+  import App from "./screens/Home.tsx";
+  import "./styles/index.css";
+
+  createRoot(document.getElementById("root")!).render(<App />);
+  
