565ce0c3bd6845ff9003944fa19ee9b852a2a747
AaKhemani <aarav.khemani@student.uts.edu.au>
2026-04-26 13:51:39 +1000

    Delete Part_B/Prototype_Design/Prototype directory

--- Part_B/Prototype_Design/Prototype/README.md
+++
@@ -1,9 +0,0 @@
-
-  # AI Triage Design Prototype
-
-  ## Running the code
-
-  Run `npm i` to install the dependencies.
-
-  Run `npm run dev` to start the development server.
-  
--- Part_B/Prototype_Design/Prototype/src/main.tsx
+++
@@ -1,7 +0,0 @@
-
-  import { createRoot } from "react-dom/client";
-  import App from "./screens/Home.tsx";
-  import "./styles/index.css";
-
-  createRoot(document.getElementById("root")!).render(<App />);
-  
